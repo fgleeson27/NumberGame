517534408efe41cc4945ea8266e5a62ec212dc10
Frank G <s1226135@monmouth.edu>
2020-03-05 20:39:15 -0500

    Version 1

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -12,17 +12,18 @@ public class GuessingGame {
 		int guess;
 		boolean win = false;
 
-		while (win == false) {
-
+		// Takes user input
 		System.out.println("Guess a number between 0 and 25: ");
 		guess = input.nextInt();
 		numberOfTries++;
 
+		//Prints "You Win" if user guesses correctly
 		if (guess == randomNumber) {
 			win = true;
 			System.out.println("You Win!");
 		}
 
+		//	Prints "You lose, the number was " and then prints the random number
 		else if (guess != randomNumber) {
 			System.out.println("You lose, the number was " + randomNumber);
 		}
@@ -30,5 +31,5 @@ public class GuessingGame {
 
 
 }
-}
+
 
